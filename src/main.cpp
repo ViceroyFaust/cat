c87d0622481acfc4ed3fa54f2736ffda0a3c20ac
Danylo Owl <d.ribchinski@gmail.com>
2021-09-14 17:45:12 -0400

    cat reads file contents

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,23 @@
-#include <stdio.h>
+#include <fstream>
+#include <iostream>
 
 int main (int argc, char* argv[]) {
-    printf("Hello World!");
+    if (argc < 2)
+        return 0;
+    
+    for (int i = 1; i < argc; i++) {
+        std::ifstream input (argv[i]);
+        // input = 0 when it could not open the file
+        if (!input) {
+            std::cout << "Error: " << argv[i] << " could not be opened\n";
+            return 0;
+        }
+        std::string output = "";
+        // As long as input has not reached EOF, it is > 0
+        while (input) {
+            std::getline(input, output);
+            std::cout << output << std::endl;
+        }
+    }
+    return 0;
 }
